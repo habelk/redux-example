77ce8bd5cba40c62678e3787d80adc907d9dde74
Habel Kurian <habel.kurian@cerner.com>
2018-02-01 12:30:06 -0600

    based on pluralsight-redux-starter

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+/**
+ * Hello World app
+ */
+import React from 'react';
+
+class App extends React.Component{
+  render() {
+    return (<div>React Testing</div>);
+  }
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+
+import App from '../src/components/App';
+import React from 'react';
+import {render} from 'react-dom';
+render(<App />, document.getElementById('app'));
